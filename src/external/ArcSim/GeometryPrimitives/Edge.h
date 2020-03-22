50351c727b34ea64fcaa621345600ad3bf1417bb
Rok Kos <rok.kos@outfit7.com>
2020-03-22 18:41:15 +0100

    Int to Bool fix

--- src/external/ArcSim/GeometryPrimitives/Edge.h
+++ src/external/ArcSim/GeometryPrimitives/Edge.h
@@ -11,7 +11,7 @@ namespace ARCSim {
 
 	struct Edge {
 		Node* n[2]; // nodes
-		int preserve;
+		bool preserve;
 		// topological data
 		Face* adjf[2]; // adjacent faces
 		int index; // position in mesh.edges
@@ -19,7 +19,7 @@ namespace ARCSim {
 		double theta_ideal, damage; // rest dihedral angle, damage parameter
 									// constructors
 		Edge() : index(-1), theta_ideal(0), damage(0) { n[0] = n[1] = 0; adjf[0] = adjf[1] = 0; }
-		explicit Edge(Node* node0, Node* node1, double theta_ideal, int preserve) :
+		explicit Edge(Node* node0, Node* node1, double theta_ideal, bool preserve) :
 			preserve(preserve), theta_ideal(theta_ideal), damage(0) {
 			n[0] = node0;
 			n[1] = node1;
