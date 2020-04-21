bbdbbd8efb760c18ad5801db45bab1729598b2b8
Rok Kos <rok.kos@outfit7.com>
2020-04-22 00:10:36 +0200

    Model Loader now support texture coordinates

--- src/Core/Primitives/ModelData.h
+++ src/Core/Primitives/ModelData.h
@@ -1,15 +1,34 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtx/hash.hpp>
 
 #include <vector>
 
+
 namespace Core {
 	
+	struct Vertex {
+		glm::vec3 pos;
+		glm::vec2 tex_coord;
+
+		bool operator==(const Vertex& other) const {
+			return pos == other.pos && tex_coord == other.tex_coord;
+		}
+	};
+
 	struct ModelData
 	{
-		std::vector<glm::vec3> vertices;
+		std::vector<Vertex> vertices;
 		std::vector<uint32_t> indices;
 	};
 
+}
+
+namespace std {
+	template<> struct hash<Core::Vertex> {
+		size_t operator()(Core::Vertex const& vertex) const {
+			return (hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec2>()(vertex.tex_coord) << 1));
+		}
+	};
 }
